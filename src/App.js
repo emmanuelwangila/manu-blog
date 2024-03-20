7c683bf647f5bda175286f35532aa9c232e9599c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-20 16:39:58 +0300

    adding output react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 
 function App() {
-  const career = "developer";
-  const jobs = "software engineer";
-
   return (
     <div className="App">
       <div className="content">
-        <h2>Manu master Rect</h2>
-        <h3>Manu is a {career} </h3>
-        <h4> Manu is a talented {jobs} </h4>
+        <h2>Mastery React</h2>
       </div>
     </div>
   );
