f5871d1fdfe044f0267aa0245f69170577ab1091
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-20 19:02:37 +0300

    mastar usestate hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [name, setName] = useState("manu");
+  const [age, setAge] = useState(10);
+
+  const handleClick = () => {
+    setName("kijana-wamalwa");
+    setAge(48);
+  };
+
+  return (
+    <div
+      style={{
+        margin: "20px",
+        display: "flex",
+        padding: "10px",
+        color: "blue",
+        fontSize: "30px",
+      }}
+    >
+      HomePage
+      <p>
+        {" "}
+        {name} is {age} years{" "}
+      </p>
+      <button onClick={handleClick}> Click Buana</button>
+    </div>
+  );
 };
 
 export default Home;
