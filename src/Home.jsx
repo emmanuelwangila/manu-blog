0d44e71e31c112c7e22389089a08ceeb1897761c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-21 19:37:46 +0300

    conditional set-time out function

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,26 +6,29 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8000/blogs");
-        if (!response.ok) {
-          throw new Error("error fetching data");
+    setTimeout(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("http://localhost:8000/blogs");
+          if (!response.ok) {
+            throw new Error("error fetching data");
+          }
+          const data = await response.json();
+          setBlogs(data);
+          setLoading(false);
+          console.log(data);
+        } catch (error) {
+          console.error("error fetching data", error.message);
         }
-        const data = await response.json();
-        setBlogs(data);
-        console.log(data);
-      } catch (error) {
-        console.error("error fetching data", error.message);
-      }
-    };
-    fetchData();
-  }, []);
+      };
+      fetchData();
+    }, 1000);
+  }, []); // Move 1000 inside the dependency array
 
   return (
     <div style={{ margin: "30px" }}>
       <h2 style={{ color: "green" }}>Home Page</h2>
-      {loading && <div>Loading </div>}
+      {loading && <div>Loading ..... </div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs Available!" />}
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.actor === "Manu")}
