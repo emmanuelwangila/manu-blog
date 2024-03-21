318ecd6affa86eb461a09dcd8805e50c421ed8d5
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-21 17:13:49 +0300

    using async await to fetch data

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,40 +2,22 @@ import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Movie Night",
-      actor: "Manu",
-      date: "JUne ",
-      genre: "Action",
-      id: 1,
-    },
-    {
-      title: "Movie",
-      actor: "Chris Evans",
-      date: "JUne ",
-      genre: "Action",
-      id: 2,
-    },
-    {
-      title: "Series",
-      actor: "Chris Evans",
-      date: "JUne ",
-      genre: "Action",
-      id: 3,
-    },
-    {
-      title: "Movie",
-      actor: "Manu",
-      date: "JUne ",
-      genre: "Action",
-      id: 4,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Use effect running");
-    console.log({ blogs });
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/blogs");
+        if (!response.ok) {
+          throw new Error("error fetching data");
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("error fetching data", error.message);
+      }
+    };
+    fetchData();
   }, []);
 
   const handleDelete = (id) => {
@@ -46,7 +28,7 @@ const Home = () => {
   return (
     <div style={{ margin: "30px" }}>
       <h2 style={{ color: "green" }}>Home Page</h2>
-      <BlogList
+      {/* <BlogList
         blogs={blogs}
         handleDelete={handleDelete}
         title="All Blogs Available!"
@@ -54,7 +36,7 @@ const Home = () => {
       <BlogList
         blogs={blogs.filter((blog) => blog.actor === "Manu")}
         title="Manu's Blogs"
-      />
+      /> */}
     </div>
   );
 };
