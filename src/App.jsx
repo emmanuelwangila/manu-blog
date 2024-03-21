01190337a78e82bc430eaaded73e0009ad9d8af9
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-21 23:37:23 +0300

    adding id route path blogs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar.jsx";
 import Home from "./Home.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create.jsx";
+import BlogDetails from "./BlogDetails.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/new-blog" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
           </Routes>
 
           {/* <h2>Mastery React</h2> */}
