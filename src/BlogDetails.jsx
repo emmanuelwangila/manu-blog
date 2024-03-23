c6813fd5ba281347ecd8e1ed501d7fbc5d036200
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-24 01:15:33 +0300

    delete function succesfully

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const history = useHistory();
+  // const history = Navigate();
 
   const { blogs, loading, error } = useFetch(
     `http://localhost:8000/blogs/${id}`
   );
 
   const handleDelete = () => {
-    fetch("http://locallhost:8000/blogs" + blogs.id, {
+    fetch("http://localhost:8000/blogs/" + blogs.id, {
       method: "DELETE",
     }).then(() => {
       console.log("Blog deleted succesfully");
-      history.push("/");
+      // history.push("/");
     });
   };
 
@@ -25,6 +25,7 @@ const BlogDetails = () => {
       <div> {loading && <div>Loading.... </div>}</div>
       {blogs && <div> {blogs.actor} </div>}
       {error && <div> {error} </div>}
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
