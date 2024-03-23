df1d7ff9060fa1cc84aa4932123bd82a9e141298
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-24 01:08:53 +0300

    delete function

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   const { blogs, loading, error } = useFetch(
     `http://localhost:8000/blogs/${id}`
   );
+
+  const handleDelete = () => {
+    fetch("http://locallhost:8000/blogs" + blogs.id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Blog deleted succesfully");
+      history.push("/");
+    });
+  };
 
   return (
     <div>
