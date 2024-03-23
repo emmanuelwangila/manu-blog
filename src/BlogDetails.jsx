a2e2ba2d6695c097990a44cb86dc36afcfb6930f
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-23 13:10:52 +0300

    I'm fixing this issue

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
 
-  return <div>BlogDetails {id}</div>;
+  const { blogs, loading, error } = useFetch("https://localhost:8000/blogs");
+
+  return (
+    <div>
+      blogdetails {id}
+      <div> error && {error} </div>
+    </div>
+  );
 };
 
 export default BlogDetails;
